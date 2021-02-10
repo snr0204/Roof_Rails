ec3d7a63b4c5611c37e4889b91d8a7f5b0419570
snr0204 <56744534+snr0204@users.noreply.github.com>
2021-02-10 11:38:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+var player ;        
+var roller ;
+var obstacle ;
+var score = 0;
+var enemiesGroup,energyGroup;
+function setup() {
+createCanvas(1000,500);
+player = createSprite(500,250,50,50);
+roller = createSprite(500,230,100,20);
+
+
+player.shapeColor = "cyan";  
+roller.shapeColor = "red";  
+
+enemiesGroup = new Group()
+energyGroup = new Group()
+
+}
+
+
+function draw() {
+	background(0)
+
+if(keyDown("left")){
+player.x=player.x-10;
+}
+if(keyDown("right")){
+player.x=player.x+10;
+}
+if(keyDown("down")){
+	player.y=player.y+10;
+	}
+	
+if(keyDown("space")){
+player.velocityY = -2;
+}
+
+
+roller.x = player.x;
+roller.y = player.y - 45;
+
+
+camera.position.y = player.y;
+camera.position.x = player.x;
+enemies()
+energy()
+
+if(enemiesGroup.collide(player)){
+score = score-10;
+enemiesGroup.destroyEach()
+}
+if(energyGroup.collide(player)){
+score = score+10;
+energyGroup.destroyEach()
+}
+
+  drawSprites();
+  textSize(22); 
+  text("Score"+score,player.x+400,player.y-200);
+  if(score===-50){
+	textSize(20);
+	text("Game Over",player.x,player.y);
+  }
+}
+function enemies (){
+	if(frameCount%150===0){
+	var x = player.x + random(-200,+200)+80;
+	var y = player.y - random(40,80)+80;
+    obstacle = createSprite(x,y,100,30);
+	obstacle.shapeColor = "green"
+	enemiesGroup.add(obstacle);
+	obstacle.lifetime = 500;
+
+	}
+}
+function energy(){
+if(frameCount%120===0){
+ var x = random(10,1000)+40;
+ var y = player.y-random(80,100)+40;
+ var e = createSprite(x,y,50,30)
+ e.shapeColor = "yellow";
+ energyGroup.add(e);
+ e.lifetime = 100;
+ 
+}
+
+
+
+
+
+}
+
+
